181af2c335318b380f9bfdfcd759bbb598d7cc83
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-02-05 22:44:19 -0300

    Update [slug].tsx

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicCLient } from "../../../services/prismic";
@@ -56,7 +56,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -87,6 +87,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post
-    }
+    },
+    redirect: 60 * 30, // 30 minutes
   }
 }
