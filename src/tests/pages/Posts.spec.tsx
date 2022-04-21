dd90dca70c5cd804a5d045fd642b5542d4748ebc
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-04-21 00:11:42 -0300

    testing the posts page

---
+++ src/tests/pages/Posts.spec.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from '@testing-library/react';
+import { mocked } from 'jest-mock';
+import Posts, { getStaticProps } from '../../pages/posts';
+import { getPrismicCLient } from '../../services/prismic'
+
+const posts = [
+  {
+    slug: 'my-new-post',
+    title: 'My New Post',
+    excerpt: 'Post excerpt',
+    updatedAt: '10 de Abril',
+  }
+];
+
+jest.mock('../../services/prismic')
+
+describe('Post page', () => {
+  it('renders correctly', () => {
+
+    render( <Posts  posts={posts} /> );
+
+    expect(screen.getByText('My New Post')).toBeInTheDocument();
+  });
+
+  it('loads initial data', async () => {
+    const getPrismicCLientMocked = mocked(getPrismicCLient);
+
+    getPrismicCLientMocked.mockReturnValueOnce({
+      get: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'my-new-post',
+            data: {
+              title: [
+                { type: 'heading', text: 'My new post' }
+              ],
+              content: [
+                { type: 'paragraph', text: 'Post excerpt'}
+              ],
+            },
+            last_publication_date: '04-01-2021',
+          }
+        ],
+      })
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [{
+            slug: 'my-new-post',
+            title: 'My new post',
+            excerpt: 'Post excerpt',
+            updatedAt: '01 de abril de 2021'
+          }]
+        },
+      })
+    );
+  });
+});
