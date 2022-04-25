2e55d6e2917f921342702d1c9bee7589757973aa
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-04-24 22:46:52 -0300

    Update [slug].tsx

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -59,7 +59,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
-    fallback: true
+    fallback: 'blocking'
   }
 }
 
